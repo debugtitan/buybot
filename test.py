fe909d17329e14329aba0c11941ffe30309f6e3d
debugtitan <alts.devs@gmail.com>
2024-05-07 20:05:49 +0100

    check liquidity test case

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from pingbot.utils.blockchain import PingSolanaClient
 
 async def main():
     PingClient = PingSolanaClient()
-    info = await PingClient.get_token_info("92TUnL8sEsSoQfp96cc9vBKV5rdbMn2wZWeXTGdr62zP")
+    info = await PingClient.get_mint_liquidity_pool_address()("92TUnL8sEsSoQfp96cc9vBKV5rdbMn2wZWeXTGdr62zP")
     print(info)
 
 asyncio.run(main())
