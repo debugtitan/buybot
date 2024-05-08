3be20abacf139f506189ba57628c809d208aad14
debugtitan <alts.devs@gmail.com>
2024-05-08 23:44:03 +0100

    resources >> pingbot.resources

--- pingbot/resources/apps.py
+++ pingbot/resources/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 class PingBotResource(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = "resources"
+    name = "pingbot.resources"
