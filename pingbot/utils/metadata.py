7b5d4d3ea90deba706be2939e5c3e7fa630003f8
debugtitan <alts.devs@gmail.com>
2024-05-08 23:27:50 +0100

    update data

--- pingbot/utils/metadata.py
+++ pingbot/utils/metadata.py
@@ -1,39 +1,45 @@
-#https://github.com/metaplex-foundation/python-api/blob/main/metaplex/metadata.py
+# https://github.com/metaplex-foundation/python-api/blob/main/metaplex/metadata.py
 
 import struct
 import base58
 
 async def unpack_metadata_account(data):
-    assert(data[0] == 4)
+    assert data[0] == 4
     i = 1
-    source_account = base58.b58encode(bytes(struct.unpack('<' + "B"*32, data[i:i+32])))
+    source_account = base58.b58encode(
+        bytes(struct.unpack("<" + "B" * 32, data[i : i + 32]))
+    )
     i += 32
-    mint_account = base58.b58encode(bytes(struct.unpack('<' + "B"*32, data[i:i+32])))
+    mint_account = base58.b58encode(
+        bytes(struct.unpack("<" + "B" * 32, data[i : i + 32]))
+    )
     i += 32
-    name_len = struct.unpack('<I', data[i:i+4])[0]
+    name_len = struct.unpack("<I", data[i : i + 4])[0]
     i += 4
-    name = struct.unpack('<' + "B"*name_len, data[i:i+name_len])
+    name = struct.unpack("<" + "B" * name_len, data[i : i + name_len])
     i += name_len
-    symbol_len = struct.unpack('<I', data[i:i+4])[0]
-    i += 4 
-    symbol = struct.unpack('<' + "B"*symbol_len, data[i:i+symbol_len])
+    symbol_len = struct.unpack("<I", data[i : i + 4])[0]
+    i += 4
+    symbol = struct.unpack("<" + "B" * symbol_len, data[i : i + symbol_len])
     i += symbol_len
-    uri_len = struct.unpack('<I', data[i:i+4])[0]
-    i += 4 
-    uri = struct.unpack('<' + "B"*uri_len, data[i:i+uri_len])
+    uri_len = struct.unpack("<I", data[i : i + 4])[0]
+    i += 4
+    uri = struct.unpack("<" + "B" * uri_len, data[i : i + uri_len])
     i += uri_len
-    fee = struct.unpack('<h', data[i:i+2])[0]
+    fee = struct.unpack("<h", data[i : i + 2])[0]
     i += 2
-    has_creator = data[i] 
+    has_creator = data[i]
     i += 1
     creators = []
     verified = []
     share = []
     if has_creator:
-        creator_len = struct.unpack('<I', data[i:i+4])[0]
+        creator_len = struct.unpack("<I", data[i : i + 4])[0]
         i += 4
         for _ in range(creator_len):
-            creator = base58.b58encode(bytes(struct.unpack('<' + "B"*32, data[i:i+32])))
+            creator = base58.b58encode(
+                bytes(struct.unpack("<" + "B" * 32, data[i : i + 32]))
+            )
             creators.append(creator)
             i += 32
             verified.append(data[i])
@@ -58,6 +64,5 @@ async def unpack_metadata_account(data):
         "primary_sale_happened": primary_sale_happened,
         "is_mutable": is_mutable,
     }
-    return metadata['data']
-
+    return metadata["data"]
 
